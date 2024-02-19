29e9ced718b4fac5e46721fabbe1e39e264988ee
Meehnaaz Basit <mhnzbasit.0002@gmail.com>
2024-02-19 21:17:10 +0600

    completed

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 let selectedSeats = 0;
-let availableSeats = 8;
+let availableSeats = 40;
 const maxSelectedSeats = 4;
 let ticketPrice = 550;
 let totalPrice = 0;
@@ -24,14 +24,15 @@ for (const seatButton of seatButtons) {
     // Increase selected seats
     selectedSeats++;
 
-    // seat
+    // Update seat information
     const remainingSeatsElement = document.getElementById("seat-remain");
     const selectedSeatsElement = document.getElementById("seat-increase");
 
+    // Inner Text
     remainingSeatsElement.innerText = availableSeats;
     selectedSeatsElement.innerText = selectedSeats;
 
-    // Mark the seat as selected
+    // Mark seat selected
     seatButton.classList.add("seat-selected");
 
     // Disable remaining seats if reached max seat
@@ -42,20 +43,16 @@ for (const seatButton of seatButtons) {
           remainingSeatButton.setAttribute("disabled", true);
         }
       }
-
       couponBtn.removeAttribute("disabled");
-      alert("You have reached the maximum number of tickets (4).");
+      alert(
+        "Thanks for reaching the 4-ticket limit! Now you can enjoy savings with your coupon code."
+      );
     }
 
-    // table data cell update///
+    // Update table data
     const table = document.getElementById("table");
-    // table body
     const tBody = document.getElementById("t-body");
-
-    // create table row
-
     const tr = document.createElement("tr");
-    // create table data
 
     const seatTd = document.createElement("td");
     seatTd.textContent = seatButton.innerText;
@@ -68,54 +65,25 @@ for (const seatButton of seatButtons) {
     const costTd = document.createElement("td");
     costTd.textContent = ticketPrice;
     tr.appendChild(costTd);
-    // console.log(td.innerText);
 
-    // append data
     tBody.appendChild(tr);
 
-    // Total price update
+    // Update total price
     totalPrice = totalPrice + ticketPrice;
     const price = document.getElementById("total-price");
     price.innerText = totalPrice;
-    // console.log(typeof totalPrice, totalPrice);
 
-    // grand total price update
+    // Update grand total price
     grandTotal = totalPrice - discountPrice;
     const grandTotalPrice = document.getElementById("grand-total");
     grandTotalPrice.innerText = grandTotal;
-
-    //--------------- next btn ------------ problem ----
-    const nextBtn = document.getElementById("next-btn");
-    const phoneNumInput = document.getElementById("phone-number-input");
-    const phoneValue = parseInt(phoneNumInput.value);
-    console.log(phoneValue);
-    if (seatButton.classList.contains("seat-selected") && phoneValue > -1) {
-      nextBtn.removeAttribute("disabled");
-    } else {
-      nextBtn.setAttribute("disabled", true);
-    }
-    // --------------------------------------------
-
-    // Alert if no seats are available
-    if (availableSeats < 0) {
-      alert("No seat is available");
-      // Changing the state
-      availableSeats++;
-      selectedSeats--;
-      remainingSeatsElement.innerText = availableSeats;
-      selectedSeatsElement.innerText = selectedSeats;
-      seatButton.classList.remove("seat-selected");
-    }
   });
 }
 
-// discount price update
-
+// Add event listener to the coupon btn
 const couponBtn = document.getElementById("coupon-btn");
-const couponInput = document.getElementById("coupon-input");
-
-// Add event listener to the coupon
 couponBtn.addEventListener("click", function () {
+  const couponInput = document.getElementById("coupon-input");
   const couponCode = couponInput.value;
   const discount = document.getElementById("discount");
   const couponSection = document.getElementById("coupon-section");
@@ -135,16 +103,30 @@ couponBtn.addEventListener("click", function () {
     alert("Invalid Coupon Code");
     couponInput.value = "";
   }
-  // grand total after applying the discount
+
+  // Update grand total after applying the discount
   grandTotal = totalPrice - discountPrice;
   const grandTotalPrice = document.getElementById("grand-total");
   grandTotalPrice.innerText = grandTotal;
 });
 
-//  Add event listener to the next button
-const nextBtn = document.getElementById("next-btn");
+// Add event listener to the phone number input
+const phoneNumInput = document.getElementById("phone-number-input");
+phoneNumInput.addEventListener("input", function () {
+  const nextBtn = document.getElementById("next-btn");
+  const phoneValue = parseInt(phoneNumInput.value);
 
+  if (selectedSeats > 0 && phoneValue !== -1 && phoneNumInput.value !== "") {
+    nextBtn.removeAttribute("disabled");
+  } else {
+    nextBtn.setAttribute("disabled", true);
+  }
+});
+
+// Add event listener to the next button
+const nextBtn = document.getElementById("next-btn");
 nextBtn.addEventListener("click", function () {
+  // console.log("clicked next-btn");
   const header = document.getElementById("header");
   const main = document.getElementById("main");
   const footer = document.getElementById("footer");
@@ -154,4 +136,11 @@ nextBtn.addEventListener("click", function () {
   main.classList.add("hidden");
   footer.classList.add("hidden");
   popUp.classList.remove("hidden");
+});
+
+// reload the page when click continue btn
+const continueBtn = document.getElementById("continue-btn");
+continueBtn.addEventListener("click", function () {
+  // console.log("clicked continue btn");
+  location.reload();
 });
